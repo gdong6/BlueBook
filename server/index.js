b04a0bf6c250b88a49341cb6261774aabc1916ae
Guoxuan <61166944+gdong6@users.noreply.github.com>
2023-01-06 18:00:13 -0500

    add all the login functions

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import postRouters from './routes/posts.js';
+import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
 
 const app = express();
 dotenv.config();
@@ -12,7 +13,8 @@ app.use(bodyParser.json({ limit: "30mb", extended: "true" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }));
 app.use(cors());
 
-app.use('/posts', postRouters);
+app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
 
 //database
 const PORT = process.env.PORT || 5001;
