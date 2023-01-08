7fcdd267548cd31af8ad5fb5d4bb1e0554db59e2
Guoxuan <61166944+gdong6@users.noreply.github.com>
2023-01-08 17:02:12 -0500

    comment part

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { getPosts, getPost, getPostsBySearch, createPost, updatePost, likePost, deletePost } from '../controllers/posts.js';
+import { getPosts, getPost, getPostsBySearch, createPost, updatePost, likePost, commentPost, deletePost } from '../controllers/posts.js';
 import auth from '../middleware/auth.js';
 
 const router = express.Router();
@@ -12,5 +12,6 @@ router.post('/', auth, createPost);
 router.patch('/:id', auth, updatePost);
 router.delete('/:id', auth, deletePost);
 router.patch('/:id/likePost', auth, likePost);
+router.post('/:id/commentPost', auth, commentPost);
 
 export default router;
